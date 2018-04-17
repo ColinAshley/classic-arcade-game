31273981006574048eb10a5dee95941e892cade7
Colin Ashley <colinashley306@gmail.com>
2018-04-17 14:28:44 +0100

    Add player/enemy movement, bounds and controls

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(xPos,yPos,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = xPos;
+    this.y = yPos;
+    // rightmost enemy position
+    this.xMax = 500;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    // if now off-screen, re-enter screen from left
+    if ( this.x >= this.xMax ) {
+        this.x = -100;
+    }
+    // TODO - Make the enemies wiggle as they walk #:o)
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,13 +35,23 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 // Enemies our player must avoid
-var Player = function() {
+var Player = function(xPos, yPos) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-boy.png';
+    // start position passed in
+    this.x = xPos;
+    this.y = yPos;
+    // min/max position limits for the player
+    this.xMin = 0;
+    this.xMax = 400;
+    this.yMin = 10;
+    this.yMax = 410;
+    this.splashSound = new Audio("audio/splash.wav");
+    this.oohSound = new Audio("audio/ooh.wav");
 };
 
 // Update the player's position, required method for game
@@ -39,21 +59,58 @@ var Player = function() {
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+    // all computers.;
 };
 
-// Draw the eplayer on the screen, required method for game
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Player.prototype.handleInput = function(dt) {
+Player.prototype.handleInput = function(keyPressed) {
     // recieve user keypress input (allowedKeys)
     // left, right, up, down
+    // Only move player if within the players bounds
+    switch (keyPressed) {
+        case 'left':
+            if (this.x > this.xMin) {
+                this.x -= 20;
+            }
+            break;
+        case 'right':
+            if (this.x < this.xMax ) {
+                this.x += 20;
+            }
+            break;
+        case 'up':
+            if (this.y <= this.yMin ) {
+                this.resetPosition();
+            }
+            else {
+                this.y -= 20;
+            }
+            break;
+        case 'down':
+            if ( this.y < this.yMax ) {
+                this.y += 20;
+            }
+            break;
+        default:
+            // nothing to do
+    }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.resetPosition = function() {
+    // place player in start position
+    // TODO - Play a splash sound
+    this.playSound(this.splashSound);
+    this.x = 200;
+    this.y = 410;
+}
+
+Player.prototype.playSound = function(audioEffect) {
+    // play a sound for the player
+    audioEffect.play();
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -68,14 +125,17 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
 // main code
-// create player object
-let player = new Player();
+// create player object in start position
+let player = new Player(200, 410);
 
-// create enemy objects
-let enemy1 = new Enemy();
-let enemy2 = new Enemy();
-let enemy3 = new Enemy();
-// add these to the allEnemies
+// create enemy objects off screen
+let enemy1 = new Enemy(-200, 65, 150);
+let enemy2 = new Enemy(-300, 145, 50);
+let enemy3 = new Enemy(-100, 225, 100);
+// add these to the allEnemies array
 let allEnemies = [enemy1, enemy2, enemy3];
-
