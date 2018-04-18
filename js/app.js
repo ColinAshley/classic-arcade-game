b1e627cbcbf16fe2725f063922899faf44a5e371
Colin Ashley <colinashley306@gmail.com>
2018-04-18 10:17:09 +0100

    set new random speed for each enemy pass

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var Enemy = function(xPos,yPos,speed) {
     this.y = yPos;
     // rightmost enemy position
     this.xMax = 500;
+    this.maxSpeed = speed;
     this.speed = speed;
 };
 
@@ -19,9 +20,14 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
+
+    // check for collision with player
+
     // if now off-screen, re-enter screen from left
+    // at a revised speed.
     if ( this.x >= this.xMax ) {
         this.x = -100;
+        this.speed = Math.floor((Math.random() * this.maxSpeed) + 50);
     }
     // TODO - Make the enemies wiggle as they walk #:o)
 };
@@ -35,81 +41,85 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 // Enemies our player must avoid
-var Player = function(xPos, yPos) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Player {
+    constructor (xPos, yPos) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
-    // start position passed in
-    this.x = xPos;
-    this.y = yPos;
-    // min/max position limits for the player
-    this.xMin = 0;
-    this.xMax = 400;
-    this.yMin = 10;
-    this.yMax = 410;
-    this.splashSound = new Audio("audio/splash.wav");
-    this.oohSound = new Audio("audio/ooh.wav");
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/char-boy.png';
+        // start position passed in
+        this.x = xPos;
+        this.y = yPos;
+        // min/max position limits for the player
+        this.xMin = 0;
+        this.xMax = 400;
+        this.yMin = 10;
+        this.yMax = 410;
+        this.splashSound = new Audio("audio/splash.wav");
+        this.giggleSound = new Audio("audio/giggle.wav");
+    }
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.;
-};
+    // Update the player's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.;
+    }
 
 // Draw the player on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-Player.prototype.handleInput = function(keyPressed) {
-    // recieve user keypress input (allowedKeys)
-    // left, right, up, down
-    // Only move player if within the players bounds
-    switch (keyPressed) {
-        case 'left':
-            if (this.x > this.xMin) {
-                this.x -= 20;
-            }
-            break;
-        case 'right':
-            if (this.x < this.xMax ) {
-                this.x += 20;
-            }
-            break;
-        case 'up':
-            if (this.y <= this.yMin ) {
-                this.resetPosition();
-            }
-            else {
-                this.y -= 20;
-            }
-            break;
-        case 'down':
-            if ( this.y < this.yMax ) {
-                this.y += 20;
-            }
-            break;
-        default:
-            // nothing to do
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-};
 
-Player.prototype.resetPosition = function() {
-    // place player in start position
-    // TODO - Play a splash sound
-    this.playSound(this.splashSound);
-    this.x = 200;
-    this.y = 410;
-}
+    handleInput(keyPressed) {
+        // recieve user keypress input (allowedKeys)
+        // left, right, up, down
+        // Only move player if within the players bounds
+        switch (keyPressed) {
+            case 'left':
+                if (this.x > this.xMin) {
+                    this.x -= 20;
+                }
+                break;
+            case 'right':
+                if (this.x < this.xMax ) {
+                    this.x += 20;
+                }
+                break;
+            case 'up':
+                if (this.y <= this.yMin ) {
+                    this.playSound(this.splashSound);
+                    this.resetPosition();
+                }
+                else {
+                    this.y -= 20;
+                }
+                break;
+            case 'down':
+                if ( this.y < this.yMax ) {
+                    this.y += 20;
+                }
+                break;
+            default:
+                // nothing to do
+        }
+    }
 
-Player.prototype.playSound = function(audioEffect) {
-    // play a sound for the player
-    audioEffect.play();
+    resetPosition() {
+        // place player in start position
+        // TODO - Play a splash sound
+        this.x = 200;
+        this.y = 410;
+    }
+
+    playSound (audioEffect) {
+        // play a sound for the player
+        audioEffect.play();
+    }
 }
 
 // This listens for key presses and sends the keys to your
@@ -134,8 +144,8 @@ document.addEventListener('keyup', function(e) {
 let player = new Player(200, 410);
 
 // create enemy objects off screen
-let enemy1 = new Enemy(-200, 65, 150);
-let enemy2 = new Enemy(-300, 145, 50);
-let enemy3 = new Enemy(-100, 225, 100);
+let enemy1 = new Enemy(-300, 65, 200);
+let enemy2 = new Enemy(-100, 145,200);
+let enemy3 = new Enemy(-200, 225, 200);
 // add these to the allEnemies array
 let allEnemies = [enemy1, enemy2, enemy3];
